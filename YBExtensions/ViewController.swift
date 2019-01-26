3aa9cc210df939e98d9c962fe6b886c4228522bb
queuey <queuey@163.com>
2019-01-26 10:19:18 +0800

    Initial Commit

---
+++ YBExtensions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YBExtensions
+//
+//  Created by yang bin on 2019/1/26.
+//  Copyright © 2019 ttttfix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
